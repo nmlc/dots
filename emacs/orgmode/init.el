f2cf04257cb19371818a5cc05875b9a7682585cc
nmlc <public.nmlc@gmail.com>
2020-04-11 06:59:59 +0500

    +

--- emacs/orgmode/init.el
+++ emacs/orgmode/init.el
@@ -16,6 +16,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(org-agenda-files (quote ("~/org/todo.org")))
  '(package-selected-packages
    (quote
     (smex ido-vertical-mode ido-completing-read+ evil org-download olivetti org-bullets poet-theme dash exec-path-from-shell use-package))))
